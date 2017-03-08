ce84ba0d29f263f9654373ac6ba37e4605880563
Soji Egbonwon <adesoji.egbonwon@cyberspace.net.ng>
2017-03-08 07:25:54 +0100

    add assignment2 project

---
+++ Documents/Visual Studio 2015/Projects/Assignment2/Assignment2/Country.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    class Country
+    {
+        public string Name;
+        public string President;
+        public float Landmass;
+        public decimal GDP;
+        public List<string> States; //store a list of states
+
+        //print out profile
+        public void PrintProfile()
+        {
+            Console.WriteLine("Name: " + Name + ", President: " + President + ", Landmass: " + Landmass);
+        }
+
+        //add a new state to existing state collection
+        public void AddState(string newstate)
+        {
+            if (States == null)
+            {
+                States = new List<string>(); //create states object
+            }
+
+            States.Add(newstate);
+        }
+
+        //print out states
+        public void PrintState()
+        {
+            foreach (string s in States)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
---
+++ Documents/Visual Studio 2015/Projects/Assignment2/Assignment2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create object of the country class
+            Country uk = new Country();
+            uk.Name = "United Kingdom";
+            uk.President = "Theresa May";
+            uk.Landmass = 945999f;
+            uk.GDP = 51.4m;
+            uk.States = new List<string> { "London", "Manchester", "Liverpool" };
+            //print out country info
+            uk.PrintProfile();
+
+            //create another object
+            Country nigeria = new Country();
+            nigeria.Name = "Nigeria";
+            nigeria.President = "Buhari";
+            nigeria.Landmass = 945999f;
+            nigeria.GDP = 51.4m;
+            nigeria.States = new List<string> { "Lagos", "Kaduna", "Owerri" };
+            nigeria.AddState("Ondo");
+            nigeria.AddState("Jigawa");
+            nigeria.PrintProfile();
+            nigeria.PrintState();
+
+            //create cameroun object
+            Country cameroun = new Country();
+            cameroun.Name = "Cameroun";
+            cameroun.President = "Biya";
+            cameroun.Landmass = 23443f;
+            cameroun.GDP = 50m;
+            cameroun.States = new List<string> { "State 1", "State 2", "State 3" };
+            cameroun.PrintProfile();
+
+            Console.ReadLine();
+        }
+    }
+}
